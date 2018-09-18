cd2de95716b83d89f81bddd9e6f934272b045fb1
Ali Naeem <alitheflyingtiger111@gmail.com>
2018-09-19 04:33:10 +0500

    tsconfig configure for testing, a simple working test inside todo.spec.ts

---
+++ dist/test/todo.spec.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("mocha");
+var assert = require("assert");
+describe("Test command", function () {
+    it("this is test", function () {
+        assert(2 + 4 === 6);
+    });
+});
+//# sourceMappingURL=todo.spec.js.map
---
+++ test/todo.spec.ts
@@ -0,0 +1,10 @@
+import "mocha";
+
+import * as assert from "assert";
+import Todo_Model from "../app/Models/Todo.model";
+
+describe("Test command", () => {
+  it("this is test", () => {
+    assert(2 + 4 === 6);
+  });
+});
